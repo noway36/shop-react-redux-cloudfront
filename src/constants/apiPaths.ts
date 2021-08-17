05aa4c86c22b2b83eac8adb344edf8000975482b
Aliaksandr Dren <Aliaksandr_Dren@epam.com>
2021-08-17 11:42:35 +0300

    feat: integrate csv import with import service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://ynbaj9n6f2.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://ynbaj9n6f2.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://ynbaj9n6f2.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://ne2vh1qshl.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://ynbaj9n6f2.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://ynbaj9n6f2.execute-api.eu-west-1.amazonaws.com/dev",
 };
