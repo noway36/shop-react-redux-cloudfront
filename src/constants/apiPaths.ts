97ac075fe435ec84ce6929d2fe66cc07f56c5e7b
Aliaksandr Dren <Aliaksandr_Dren@epam.com>
2021-08-30 19:35:46 +0300

    attach new import stack path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://ynbaj9n6f2.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://ynbaj9n6f2.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://ne2vh1qshl.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://ynbaj9n6f2.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://ynbaj9n6f2.execute-api.eu-west-1.amazonaws.com/dev",
+    product: "https://ynbaj9n6f2.execute-api.eu-west-1.amazonaws.com/dev",
+    order: "https://ynbaj9n6f2.execute-api.eu-west-1.amazonaws.com/dev",
+    import: "https://zekwn0wsp3.execute-api.eu-west-1.amazonaws.com/dev",
+    bff: "https://ynbaj9n6f2.execute-api.eu-west-1.amazonaws.com/dev",
+    cart: "https://ynbaj9n6f2.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
