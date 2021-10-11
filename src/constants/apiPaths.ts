333a7978260ecab55818aa82eff0f86e81523bc6
Aliaksandr Dren <Aliaksandr_Dren@epam.com>
2021-10-11 23:00:00 +0300

    feat: use bff instead of services for products and cart

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,8 +2,8 @@ const API_PATHS = {
     product: "https://ynbaj9n6f2.execute-api.eu-west-1.amazonaws.com/dev",
     order: "https://ynbaj9n6f2.execute-api.eu-west-1.amazonaws.com/dev",
     import: "https://zekwn0wsp3.execute-api.eu-west-1.amazonaws.com/dev",
-    bff: "https://ynbaj9n6f2.execute-api.eu-west-1.amazonaws.com/dev",
-    cart: "http://noway36-cart-api-develop-1.eu-west-1.elasticbeanstalk.com/api",
+    bff: "http://noway36-bff-api-develop-1.eu-west-1.elasticbeanstalk.com",
+    cart: "http://noway36-bff-api-develop-1.eu-west-1.elasticbeanstalk.com",
 };
 
 export default API_PATHS;
